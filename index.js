b68cd1b1142a15b21ec5fe5ca404df2cf9718514
kriztiank <kriztiank@gmail.com>
2020-01-29 13:27:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+// const expressLayouts = require("express-ejs-layouts");
+
+app.set('view engine', 'ejs');
+// app.use(expressLayouts);
+app.use(express.static('./'));
+
+app.get('/', (req, res) => {
+  res.render('pages/index', {
+    title: 'Sidetitel',
+    content: 'Side Indholdet',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Express kører på port 3000');
+});
