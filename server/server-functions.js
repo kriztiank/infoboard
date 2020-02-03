6495e2a7776741dcb48e521149a0f964ef556c04
TomGrooves <kasperfryd@gmail.com>
2020-02-03 13:13:44 +0100

    Updated 'determineContent' for finer control

--- server/server-functions.js
+++ server/server-functions.js
@@ -22,33 +22,49 @@ var functionCollection = {
     act_arr_4: [], // 12.00 - 13.00
     act_arr_5: [], // 13.00 - 14.00
     act_arr_6: [], // 14.00 - 15.15
+    act_arr_7: [], // 15.15 - 16.00
 
     // Function to sort data by time and date
     sort: function (el) {
         for (var x = 0; x < el.length; x++) {
+            //console.log(getItemDate(act.daTime) + " " + getTodaysDate())
 
             if (this.getItemDate(el[x].stamp) == this.getTodaysDate()) {
 
+                // kl 8
                 if (this.getHours(el[x].stamp) == 8) {
                     this.act_arr_1.push(el[x]);
                 }
+                
+                // kl 9
                 if (this.getHours(el[x].stamp) == 9) {
                     this.act_arr_2.push(el[x]);
                 }
+
+                // kl 10
                 if (this.getHours(el[x].stamp) == 10) {
                     this.act_arr_3.push(el[x]);
                 }
+
+                // kl 11
                 if (this.getHours(el[x].stamp) == 11) {
                     this.act_arr_3.push(el[x]);
                 }
+                // kl 12
                 if (this.getHours(el[x].stamp) == 12) {
                     this.act_arr_4.push(el[x]);
                 }
+                // kl 13
                 if (this.getHours(el[x].stamp) == 13) {
                     this.act_arr_5.push(el[x]);
                 }
+                // kl 14
                 if (this.getHours(el[x].stamp) == 14) {
                     this.act_arr_6.push(el[x]);
+                }
+                // kl 15
+                if (this.getHours(el[x].stamp) == 15){
+                    this.act_arr_7.push(el[x]);
                 }
             }
         };
@@ -57,81 +73,77 @@ var functionCollection = {
     // Function to determine what content to show based on currentime
     determineContent: function () {
         var hour = this.getCurrentHour();
-        //console.log("Server updated at: " + new Date)
-        switch (hour) {
-            case 8:
-                this.content = this.act_arr_1;
-                break;
-
-            case 9:
+        var min = this.getMinutes();
+        console.log("Server updated at hour: " + hour + "Min: " + min );
+        if (hour == 8 || hour == 9 && min < 20){
+            this.content = this.act_arr_1;
+        }
+        if (hour == 9 && min >=20 || hour == 10 && min < 30){
                 this.content = this.act_arr_2;
-                break;
-
-            case 10:
+        }
+        if (hour == 10 && min >=30 || hour == 11 && min < 30){
                 this.content = this.act_arr_3;
-                break;
-
-            case 11:
+        }
+        if (hour == 11 && min >=30 || hour == 12){
                 this.content = this.act_arr_4;
-                break;
-
-            case 12:
-                this.content = this.act_arr_4;
-                break;
-
-            case 13:
+        }
+        if (hour == 13){
                 this.content = this.act_arr_5;
-                break;
-
-            case 14:
+        }
+        if (hour == 14){
                 this.content = this.act_arr_6;
-                break;
-
-            default:
-                this.content = this.act_arr_1;
-                
-                break;
+        }
+        if (hour == 15 && min > 15){
+                this.content = this.act_arr_7;
         }
     },
 
     // Function to get the current hour of the item
     getHours: function (time, add = 0) {
-        var date = new Date(time * 1000);
-        var hours = date.getHours() + add;
-        var formatted = hours;
+        let date = new Date(time * 1000);
+        let hours = date.getHours() + add;
+        let formatted = hours;
+        return formatted;
+    },
+
+    // Function to get the current hour of the item
+    getMinutes: function () {
+        let date = new Date();
+        let min = date.getMinutes();
+        let formatted = min;
         return formatted;
     },
 
     // Function to get the current date on the item in d/m/y
     getItemDate: function (time) {
-        var date = new Date(time * 1000);
-        var day = date.getDate();
-        var month = date.getMonth() + 1;
-        var year = date.getFullYear();
-        var formatted = day + "/" + month + "/" + year;
+        let date = new Date(time * 1000);
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        let formatted = day + "/" + month + "/" + year;
         return formatted;
     },
 
     // Function to get the current time of the day
     getCurrentHour: function () {
-        var date = new Date;
-        var hours = date.getHours();
-        var formatted = hours;
+        let date = new Date;
+        let hours = date.getHours();
+        let formatted = hours;
         return formatted;
     },
 
     // Function that returns the current date in format d/m/y
     getTodaysDate: function () {
-        var date = new Date;
+        let date = new Date;
         let shift = 0;
         if (this.getCurrentHour() > 16){
             shift = 1;
             console.log("Day shifted");
         }
-        var day = date.getDate() + shift;
-        var month = date.getMonth() + 1;
-        var year = date.getFullYear();
-        var formatted = day + "/" + month + "/" + year;
+        let day = date.getDate() + shift;
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        let formatted = day + "/" + month + "/" + year;
         return formatted;
     },
 
